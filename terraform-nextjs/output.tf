f5b38eaf2b8cf3701f9326ec3bef716164627efc
Dibrova N. <dnatalyaalex@gmail.com>
2026-06-18 17:45:15 +0200

    README file has been updated

--- terraform-nextjs/output.tf
+++ terraform-nextjs/output.tf
@@ -1,7 +1,3 @@
-output "bucket_website_endpoint" {
-  value = aws_s3_bucket.s3static.website_endpoint
-}
-
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
